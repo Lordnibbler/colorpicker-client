40862b351dd2a5c31339f750ed0c687ca6d50199
root <root@beaglebone.(none)>
2014-08-17 22:28:31 -0700

    update wifiboot

--- wifiboot/wifiboot.sh
+++ wifiboot/wifiboot.sh
@@ -2,13 +2,23 @@
 sleep 10
 #systemctl restart connman.service
 
+#enable the UART1 in the device tree
+echo BB-UART1 > /sys/devices/bone_capemgr.9/slots
+
+
+#Set proper UART settings with STTY
+
+stty -F /dev/ttyO1 speed 115200 ignbrk -brkint -icrnl -imaxbel -opost
+-onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh
+-ixon -crtscts
+
 #python ping_test.py >/dev/null &
 
 cd /home/root/halo_git/
 
-./Halo_Master.pl > /dev/null & 
+#./Halo_Master.pl > /dev/null &
 
 cd /home/root/colorpicker-beaglebone/
 
-NODE_ENV=production npm start
+#NODE_ENV=production npm start
 
