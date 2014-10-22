84aa76491dcb5f513279420469874920ef4a8ed0
Ben Radler <benradler@me.com>
2014-10-21 21:26:40 -0700

    Update readme and angstrom systemctl service templates

--- angstrom/colorpickerbeaglebone.sh
+++ angstrom/colorpickerbeaglebone.sh
@@ -7,7 +7,6 @@ echo BB-UART1 > /sys/devices/bone_capemgr.9/slots
 # Set proper UART settings with STTY
 stty -F /dev/ttyO1 speed 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 
-cd /home/root/colorpicker-beaglebone/
-
 # start the node client application
+cd /home/root/colorpicker-beaglebone/
 NODE_ENV=production npm start
